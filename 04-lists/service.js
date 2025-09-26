65f6e7877e116ddea2e5e1c024fd28359ae724e3
José Neto <jdirceuhn33@gmail.com>
2025-09-25 22:27:29 -0300

    node_modules removed

--- 04-lists/service.js
+++ 04-lists/service.js
@@ -1,8 +1,8 @@
 const axios = require("axios");
-const URL = `https://swapi.co/api/people`;
+const URL = `https://swapi.dev/api/people`;
 
 async function obterPessoas(nome) {
-  const url = `${URL}/?search=${nome}&format=json`;
+  const url = `${URL}/?search=${nome}`;
   const response = await axios.get(url);
   return response.data;
 }
